ab26785d32e046186249020c391ae58e71a3d3c3
Ryuseo <rkd4apf@gmail.com>
2016-10-18 03:58:13 +0900

    이게 진짜 마지막 중간 발표전 커밋
    과연...

--- code/main_state.py
+++ code/main_state.py
@@ -13,6 +13,9 @@ import title_state
 name = "MainState"
 
 player = None
+cdealer = None
+mdealer = None
+healer = None
 map = None
 font = None
 mob_dog = None
@@ -23,10 +26,24 @@ inputw,inputa,inputs,inputd,input_tab,input_pskill,p_atk = False,False,False,Fal
 class Map:
     def __init__(self):
         self.image = load_image('map.png')
-        self.x, self.y = 32, 32
+        self.x, self.y = 480, 320
+        self.left, self.bottom = 0,0
     def draw(self):
-        self.image.clip_draw(0, 0,960, 640, 480, 320)
+        self.image.clip_draw(self.left, self.bottom,960, 640, self.x, self.y)
+    def update(self):
+        if inputw == True:
+            if self.bottom < 320:
+                self.bottom = self.bottom + 1
+        elif inputs == True:
+            if self.bottom > 0:
+                self.bottom = self.bottom - 1
 
+        if inputd == True:
+            if self.left < 0:
+                self.left = self.left + 1
+        elif inputa == True:
+            if self.left > 0:
+                self.left = self.left - 1
 
 
 class Player:
@@ -39,8 +56,8 @@ class Player:
         self.tx, self.ty = None, None
         self.frame = 0
         self.dir = 0
-        self.jop = 0
         self.atk = False
+        self.target = None
         self.image = load_image('tanker.png')
         self.image_t = load_image('target.png')
         self.image_atk = load_image('tanker_atk1.png')
@@ -78,6 +95,7 @@ class Player:
 
         if input_pskill == 1:
             if temp3 < 28:
+                self.target = mob_dog
                 self.atk = True
                 input_pskill = 0
 
@@ -101,7 +119,6 @@ class Player:
             self.atk = False
 
 
-
 class Mob_dog:
     image = None
     global player
@@ -118,15 +135,16 @@ class Mob_dog:
            Mob_dog.image = load_image('dog.png')
 
     def update(self):
+        global player
         self.frame = (self.frame + 1) % 3
         if self.target == None:
             temp1 = pow(player.x - self.x, 2)
             temp2 = pow(player.y - self.y, 2)
             temp3 = sqrt(temp1 + temp2)
             if temp3 < 100:
-                self.target = Player
+                self.target = player
                 self.p_agg = 1
-        elif self.target == Player:
+        elif self.target == player:
             temp1 = pow(player.x - self.x, 2)
             temp2 = pow(player.y - self.y, 2)
             temp3 = sqrt(temp1 + temp2)
@@ -147,18 +165,246 @@ class Mob_dog:
         self.image.clip_draw(self.frame * 32, self.dir * 32, 32, 32, self.x, self.y)
 
 
+class CDealer:
+    def __init__(self):
+        self.x, self.y = 60, 60
+        self.vx, self.vy = 0, 0
+        self.tx, self.ty = 0,0
+        self.frame = 0
+        self.dir = 0
+        self.atk = False
+        self.atkc = 0
+        self.target = None
+        self.image = load_image('close_dealer.png')
+        self.image_atk = load_image('cdealer_atk1.png')
+
+    def update(self):
+        global player, mob_dog
+        self.frame = (self.frame + 1) % 3
+
+        if player.target != None:
+            self.target = player.target
+            self.tx = self.target.x
+            self.ty = self.target.y
+            self.atc = 0
+
+        if self.target == None:
+
+            temp1 = pow(player.x - self.x, 2)
+            temp2 = pow(player.y - self.y, 2)
+            temp3 = sqrt(temp1 + temp2)
+
+            if temp3 < 23:
+                self.vy = 0
+                self.vx = 0
+            else:
+                temp1 = player.x - self.x
+                temp2 = player.y - self.y
+                self.vx = temp1 / temp3
+                self.vy = temp2 / temp3
+        elif self.target == mob_dog:
+
+            temp1 = pow(mob_dog.x - self.x, 2)
+            temp2 = pow(mob_dog.y - self.y, 2)
+            temp3 = sqrt(temp1 + temp2)
+
+            if temp3 < 23:
+                self.vy = 0
+                self.vx = 0
+            else:
+                temp1 = mob_dog.x - self.x
+                temp2 = mob_dog.y - self.y
+                self.vx = temp1 / temp3
+                self.vy = temp2 / temp3
+
+            if temp3 < 30:
+                self.atk = True
+                self.tx = self.target.x
+                self.ty = self.target.y
+
+        self.x = self.x + self.vx
+        self.y = self.y + self.vy
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 32, self.dir * 32, 32, 32, self.x, self.y)
+
+        if self.atk == True:
+            self.image_atk.draw(self.tx,self.ty)
+            self.atkc = self.atkc + 1
+            if self.atkc == 10:
+                self.atkc = 0
+                self.atk = False
+        else:
+            self.atk = False
+
+
+class MDealer:
+    def __init__(self):
+        self.x, self.y = 30, 60
+        self.vx, self.vy = 0, 0
+        self.tx, self.ty = 0,0
+        self.frame = 0
+        self.dir = 0
+        self.atk = False
+        self.atkc = 0
+        self.target = None
+        self.image = load_image('magic_dealer.png')
+        self.image_atk = load_image('fire.png')
+
+    def update(self):
+        global player, mob_dog
+        self.frame = (self.frame + 1) % 3
+
+        if player.target != None:
+            self.target = player.target
+            self.tx = self.target.x
+            self.ty = self.target.y
+            self.atc = 0
+
+        if self.target == None:
+
+            temp1 = pow(player.x - self.x, 2)
+            temp2 = pow(player.y - self.y, 2)
+            temp3 = sqrt(temp1 + temp2)
+
+            if temp3 < 23:
+                self.vy = 0
+                self.vx = 0
+            else:
+                temp1 = player.x - self.x
+                temp2 = player.y - self.y
+                self.vx = temp1 / temp3
+                self.vy = temp2 / temp3
+        elif self.target == mob_dog:
+
+            temp1 = pow(mob_dog.x - self.x, 2)
+            temp2 = pow(mob_dog.y - self.y, 2)
+            temp3 = sqrt(temp1 + temp2)
+
+            if temp3 < 23:
+                self.vy = 0
+                self.vx = 0
+            else:
+                temp1 = mob_dog.x - self.x
+                temp2 = mob_dog.y - self.y
+                self.vx = temp1 / temp3
+                self.vy = temp2 / temp3
+
+            if temp3 < 100:
+                self.atk = True
+                self.vx = 0
+                self.vy = 0
+                self.tx = self.target.x
+                self.ty = self.target.y
+
+        self.x = self.x + self.vx
+        self.y = self.y + self.vy
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 32, self.dir * 32, 32, 32, self.x, self.y)
+
+        if self.atk == True:
+            self.image_atk.clip_draw(self.frame * 32, 0, 32, 32, self.tx, self.ty)
+            self.atkc = self.atkc + 1
+            if self.atkc == 10:
+                self.atkc = 0
+                self.atk = False
+        else:
+            self.atk = False
+
+
+class Healer:
+    def __init__(self):
+        self.x, self.y = 30, 60
+        self.vx, self.vy = 0, 0
+        self.tx, self.ty = 0,0
+        self.frame = 0
+        self.dir = 0
+        self.atk = False
+        self.atkc = 0
+        self.target = None
+        self.image = load_image('healer.png')
+        self.image_atk = load_image('heal.png')
+
+    def update(self):
+        global player
+        self.frame = (self.frame + 1) % 3
+
+        if player.target != None:
+            self.target = player
+            self.tx = self.target.x
+            self.ty = self.target.y
+            self.atc = 0
+
+        if self.target == None:
+
+            temp1 = pow(player.x - self.x, 2)
+            temp2 = pow(player.y - self.y, 2)
+            temp3 = sqrt(temp1 + temp2)
+
+            if temp3 < 23:
+                self.vy = 0
+                self.vx = 0
+            else:
+                temp1 = player.x - self.x
+                temp2 = player.y - self.y
+                self.vx = temp1 / temp3
+                self.vy = temp2 / temp3
+        elif self.target == player:
+
+            temp1 = pow(mob_dog.x - self.x, 2)
+            temp2 = pow(mob_dog.y - self.y, 2)
+            temp3 = sqrt(temp1 + temp2)
+
+            if temp3 < 23:
+                self.vy = 0
+                self.vx = 0
+            else:
+                temp1 = mob_dog.x - self.x
+                temp2 = mob_dog.y - self.y
+                self.vx = temp1 / temp3
+                self.vy = temp2 / temp3
+
+            if temp3 < 50:
+                #self.atk = True
+                self.vx = 0
+                self.vy = 0
+                self.tx = self.target.x
+                self.ty = self.target.y
+
+        self.x = self.x + self.vx
+        self.y = self.y + self.vy
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 32, self.dir * 32, 32, 32, self.x, self.y)
+
+        if self.atk == True:
+            self.image_atk.clip_draw(self.frame * 32, 0, 32, 32, self.tx, self.ty)
+            self.atkc = self.atkc + 1
+            if self.atkc == 10:
+                self.atkc = 0
+                self.atk = False
+        else:
+            self.atk = False
+
+
 def enter():
-    global player, map, mob_dog
+    global player, map, mob_dog,cdealer,mdealer, healer
     player = Player()
     map = Map()
     mob_dog = Mob_dog()
+    cdealer = CDealer()
+    mdealer = MDealer()
+    healer = Healer()
 
 
 def exit():
-    global boy, grass
+    global boy, grass, cdealer, mdealer, healer
     del(boy)
+    del(cdealer)
     del(grass)
     del(mob_dog)
+    del(healer)
 
 def pause():
     pass
@@ -204,12 +450,19 @@ def handle_events():
 
 def update():
     player.update()
+    cdealer.update()
+    mdealer.update()
     mob_dog.update()
+    healer.update()
+    map.update()
 
 
 def draw():
     clear_canvas()
     map.draw()
     mob_dog.draw()
+    cdealer.draw()
+    mdealer.draw()
+    healer.draw()
     player.draw()
     update_canvas()
